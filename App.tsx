16de160b1dfce6d81baf8ab8ac1517f32fcfd39f
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-04 19:02:05 +0000

    Add native module

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NativeModules } from "react-native";
+// const {CalendarModule} = NativeModules;
 
 export default function App() {
+  NativeModules.LiveActivityModule.createCalendarEvent("A", "B");
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
