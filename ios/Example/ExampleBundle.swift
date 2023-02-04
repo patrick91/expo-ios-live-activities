8cd09b92fa570fac314f44d006ee53e554e4b5a1
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-04 15:58:10 +0000

    Add widget kit

---
+++ ios/Example/ExampleBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleBundle.swift
+//  Example
+//
+//  Created by Patrick Arminio on 04/02/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ExampleBundle: WidgetBundle {
+    var body: some Widget {
+        Example()
+        ExampleLiveActivity()
+    }
+}
